6f7c796ae3df7050fae046398ac3c162965f1709
Subhashchandra G Borad <subhashchandraborad3295@gmail.com>
2026-04-10 09:11:34 +0200

    Remove Languaga and update experince

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -126,7 +126,6 @@ const resume = () => {
             <span className="px-2">|</span>JQuery
             <span className="px-2">|</span>Angular
             <span className="px-2">|</span>React
-            <span className="px-2">|</span>Vue
             <span className="px-2">|</span>NextJS
             <span className="px-2">|</span>Javascript
             <span className="px-2">|</span>Typescript
